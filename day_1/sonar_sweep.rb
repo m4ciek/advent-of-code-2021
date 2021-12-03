5f82c542978a6357ad62ffd5faa3bd8b171129ba
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-03 12:41:26 -0700

    bit of code golf. I love me some `ARGF` yo.

--- day_1/sonar_sweep.rb
+++ day_1/sonar_sweep.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
 
-depth_measurements = []
-
-ARGF.each do |ping|
-    depth_measurements << ping.to_i
-end
-
+depth_measurements = ARGF.map(&:to_i)
 trends = depth_measurements.each_cons(2).map { |pings| pings.reduce(:-) <=> 0 }.tally
 
 puts trends.fetch(-1)
