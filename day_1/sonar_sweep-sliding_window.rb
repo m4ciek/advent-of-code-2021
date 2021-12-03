132a2b2f07b610a45109c1d6c60fce4a7c641ffa
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-03 12:49:33 -0700

    sliding window solution

---
+++ day_1/sonar_sweep-sliding_window.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+depth_measurements = ARGF.map(&:to_i)
+trends = depth_measurements.each_cons(3).map(&:sum).each_cons(2).map { |pings| pings.reduce(:-) <=> 0 }.tally
+
+puts trends.fetch(-1)
