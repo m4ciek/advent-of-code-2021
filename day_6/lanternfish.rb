b704730a68afccf73af0dd9f3335c4b9a6b74caa
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-09 10:57:59 -0700

    hah, ridiculous codegolfing. rotate unnecessary. fancy variable swap with increment.

--- day_6/lanternfish.rb
+++ day_6/lanternfish.rb
@@ -1,33 +1,30 @@
 #!/usr/bin/env ruby
 
-MAX_STATE = 6
+MAX_STATE_PLUS_ONE = 7
 
-popl_initial = (
-  ('0'..MAX_STATE.to_s).map { |i| [i, 0] } +
-    ARGF.read.strip.split(',').tally.to_a
-).group_by(&:first).map do |_k, v|
-  v.map(&:last).sum
-end
+popl_initial =
+  MAX_STATE_PLUS_ONE.times.zip(Enumerator.produce { 0 }).to_h.merge(
+    ARGF.read.split(',').map(&:to_i).tally
+  ).values_at(*MAX_STATE_PLUS_ONE.times)
 
 puts(
   [80, 256].map.with_index do |days, run_number|
     popl = popl_initial.dup
-    juveniles = [0,0]
-    days.times do
-      new_adults = juveniles[0]
-      juveniles[0] = popl[0]
-      popl[0] += new_adults
-      juveniles.rotate!
-      popl.rotate!
+    juveniles = [0, 0]
+
+    days.times do |day|
+      j_cyc = day % 2
+      popl_cyc = day % MAX_STATE_PLUS_ONE
+      juveniles[j_cyc], popl[popl_cyc] = [popl[popl_cyc], popl[popl_cyc] + juveniles[j_cyc]]
     end
 
     format(
-      "part %d:\ttotal is %d (%d mature, %d juvenile; simulation ran %d simulated days)",
-      run_number,
-      popl.sum + juveniles.last(2).sum,
+      "part %d: total is %d\n\t* %d mature\n\t* %d juvenile\n\t* simulation ran %d simulated days",
+      run_number.succ,
+      popl.sum + juveniles.sum,
       popl.sum,
-      juveniles.last(2).sum,
+      juveniles.sum,
       days
     )
-  end.join("\n")
+  end.join("\n\n")
 )
