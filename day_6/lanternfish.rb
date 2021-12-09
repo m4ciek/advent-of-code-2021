da780bbf6f6f30f56a3fccb0532963b2b637611a
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-09 13:11:44 -0700

    the new cleverness is a travesty. undo it, and tweak formatting. ok. done.

--- day_6/lanternfish.rb
+++ day_6/lanternfish.rb
@@ -10,22 +10,27 @@ popl =
 
 juveniles = [0, 0]
 
-puts(loop.with_index.with_object([]) do |(_, day), report|
-  if REPORT_AT.include?(day)
-    report << format(
-      "part %d: total is %d\n\t* %d mature\n\t* %d juvenile\n\t* simulation ran %d simulated days",
-      report.size.succ,
-      popl.sum + juveniles.sum,
-      popl.sum,
-      juveniles.sum,
-      day
-    )
+puts(
+  loop.with_index.with_object([]) do |(_, day), report|
+    if REPORT_AT.include?(day)
+      report << format(
+        "part %d: total is %d\n\t* %d mature\n\t* %d juvenile\n\t* simulation ran %d simulated days",
+        report.size.succ,
+        popl.sum + juveniles.sum,
+        popl.sum,
+        juveniles.sum,
+        day
+      )
+    end
+
+    break report.join("\n\n") if REPORT_AT.size == report.size
+
+    j_cyc = day % 2
+    popl_cyc = day % MAX_STATE_PLUS_ONE
+
+    juveniles[j_cyc], popl[popl_cyc] = [
+      popl[popl_cyc],
+      popl[popl_cyc] + juveniles[j_cyc]
+    ]
   end
-
-  break report.join("\n\n") if REPORT_AT.size == report.size
-
-  j_cyc = day % 2
-  popl_cyc = day % MAX_STATE_PLUS_ONE
-  # this is just showing off, I completely admit 🤓
-  juveniles[j_cyc], popl[popl_cyc] = popl.values_at(popl_cyc).cycle(2).zip([0, juveniles[j_cyc]]).map(&:sum)
-end)
+)
