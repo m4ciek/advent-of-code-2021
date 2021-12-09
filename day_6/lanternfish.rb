e78bafdaf0cd84b343d38f71678e51f189d493e1
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-09 13:06:16 -0700

    optimize reporting (don't re-run); nerd (🤓) cred

--- day_6/lanternfish.rb
+++ day_6/lanternfish.rb
@@ -1,30 +1,31 @@
 #!/usr/bin/env ruby
 
 MAX_STATE_PLUS_ONE = 7
+REPORT_AT = [80, 256]
 
-popl_initial =
+popl =
   MAX_STATE_PLUS_ONE.times.zip(Enumerator.produce { 0 }).to_h.merge(
     ARGF.read.split(',').map(&:to_i).tally
   ).values_at(*MAX_STATE_PLUS_ONE.times)
 
-puts(
-  [80, 256].map.with_index do |days, run_number|
-    popl = popl_initial.dup
-    juveniles = [0, 0]
+juveniles = [0, 0]
 
-    days.times do |day|
-      j_cyc = day % 2
-      popl_cyc = day % MAX_STATE_PLUS_ONE
-      juveniles[j_cyc], popl[popl_cyc] = [popl[popl_cyc], popl[popl_cyc] + juveniles[j_cyc]]
-    end
-
-    format(
+puts(loop.with_index.with_object([]) do |(_, day), report|
+  if REPORT_AT.include?(day)
+    report << format(
       "part %d: total is %d\n\t* %d mature\n\t* %d juvenile\n\t* simulation ran %d simulated days",
-      run_number.succ,
+      report.size.succ,
       popl.sum + juveniles.sum,
       popl.sum,
       juveniles.sum,
-      days
+      day
     )
-  end.join("\n\n")
-)
+  end
+
+  break report.join("\n\n") if REPORT_AT.size == report.size
+
+  j_cyc = day % 2
+  popl_cyc = day % MAX_STATE_PLUS_ONE
+  # this is just showing off, I completely admit 🤓
+  juveniles[j_cyc], popl[popl_cyc] = popl.values_at(popl_cyc).cycle(2).zip([0, juveniles[j_cyc]]).map(&:sum)
+end)
