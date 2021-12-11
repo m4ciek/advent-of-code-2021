f1c320c949d31561903f6f5ec691b2a70e71ccaf
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-10 21:31:58 -0700

    yikes, takes a minute to run on my new Mac 😳

--- day_8/cracked_lcd.rb
+++ day_8/cracked_lcd.rb
@@ -1,34 +1,47 @@
 #!/usr/bin/env ruby
 
-# okay, LEDs
-#
-# a:  0, 2, 3, 5, 6, 7, 8, 9
-# b:  0, 4, 5, 6, 8, 9
-# c:  0, 1, 2, 3, 4, 7, 8, 9
-# d:  2, 3, 4, 5, 6, 8, 9
-# e:  0, 2, 6, 8
-# f:  0, 1, 3, 4, 5, 6, 7, 8, 9
-# g:  0, 2, 3, 5, 6, 8, 9
+numbers =
+  ARGF.readlines.map do |line|
+    line.partition('|').values_at(0,2).map do |part|
+      part.split.map(&:chars).map(&:sort)
+    end
+  end
 
-# copy the preceding into the clipboard, then:
+solution_one = numbers.flat_map(&:last).group_by(&:size).values_at(2, 3, 4, 7).map(&:size).sum
 
-# [55] pry(main)> letter_to_numbers=Clipboard.paste.split("\n").map do |seg| letter, numbers = /\A# ([^:]+):\s*(.+)\z/.match(seg).captures; [letter, numbers.scan(/[[:digit:]]+/).map(&:to_i)] end
-# => [["a", [0, 2, 3, 5, 6, 7, 8, 9]],
-#  ["b", [0, 4, 5, 6, 8, 9]],
-#  ["c", [0, 1, 2, 3, 4, 7, 8, 9]],
-#  ["d", [2, 3, 4, 5, 6, 8, 9]],
-#  ["e", [0, 2, 6, 8]],
-#  ["f", [0, 1, 3, 4, 5, 6, 7, 8, 9]],
-#  ["g", [0, 2, 3, 5, 6, 8, 9]]]
-# [56] pry(main)> letter_to_numbers.flat_map do |letter, numbers| numbers.map do |number| [number, letter] end end.group_by(&:first).map do |k,v| [k,v.flat_map(&:last)] end.sort do |a,b| a.last.size<=>b.last.size end
-# => [[1, ["c", "f"]],
-#  [7, ["a", "c", "f"]],
-#  [4, ["b", "c", "d", "f"]],
-#  [5, ["a", "b", "d", "f", "g"]],
-#  [3, ["a", "c", "d", "f", "g"]],
-#  [2, ["a", "c", "d", "e", "g"]],
-#  [9, ["a", "b", "c", "d", "f", "g"]],
-#  [6, ["a", "b", "d", "e", "f", "g"]],
-#  [0, ["a", "b", "c", "e", "f", "g"]],
-#  [8, ["a", "b", "c", "d", "e", "f", "g"]]]
-# [57] pry(main)>
+puts "part 1: #{solution_one}"
+
+NUMBERS = [
+  %w[a b c e f g],
+  %w[c f],
+  %w[a c d e g],
+  %w[a c d f g],
+  %w[b c d f],
+  %w[a b d f g],
+  %w[a b d e f g],
+  %w[a c f],
+  %w[a b c d e f g],
+  %w[a b c d f g]
+]
+
+solution_two =
+  ('a'..'g').to_a.permutation.flat_map do |p|
+    mapping = p.zip('a'..'g').to_h
+    numbers.map do |patterns, group_of_four|
+      next unless (
+          NUMBERS -
+          patterns.map { |pattern| mapping.values_at(*pattern).sort }
+        ).empty?
+
+      group_of_four.map do |one_of_four|
+        NUMBERS.index(mapping.values_at(*one_of_four).sort)
+      end
+    end.compact
+  end
+
+printf(
+  "part 2: %d\n",
+  solution_two.map do |digits|
+    digits.reverse.map.with_index { |digit, mag| digit * 10**mag }.sum
+  end.sum
+)
